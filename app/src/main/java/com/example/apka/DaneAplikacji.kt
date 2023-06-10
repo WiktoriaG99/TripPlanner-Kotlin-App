09e160b35c2d3bcc62fb7f8373ea8ead5baacb26
WiktoriaG99 <wiktoriagajda99@gmail.com>
2023-06-10 19:39:14 +0200

    adding functionality między innymi to że wylicza liczbe przedmiotow na podstawie dat

--- app/src/main/java/com/example/apka/DaneAplikacji.kt
+++ app/src/main/java/com/example/apka/DaneAplikacji.kt
@@ -4,14 +4,14 @@ class DaneAplikacji {
 
     //Lista list - Podróże, zawierające pojedyncze elementy Podroz
     public var Podroze = mutableListOf<Podroz>(
-        Podroz(0, "Wakacje", "01.06.2023", "07.06.2023",
+        Podroz(0, "Wakacje", "01-06-2023", "07-06-2023",
         "Katowice", "Rekreacja",
             mutableListOf<Przedmiot>(
                 Przedmiot("Szampon", 1, false, false),
                 Przedmiot("Dokumenty", 1, false, false)
             )
         ),
-        Podroz(1, "Podroz", "01.06.2023", "07.06.2023",
+        Podroz(1, "Podroz", "01-06-2023", "07-06-2023",
             "Rybnik", "Rekreacja",
             mutableListOf<Przedmiot>(
                 Przedmiot("Torebka", 1, false, false),
--- app/src/main/java/com/example/apka/UtworzElement.kt
+++ app/src/main/java/com/example/apka/UtworzElement.kt
@@ -1,14 +1,20 @@
 package com.example.apka
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.example.apka.databinding.ActivityMainBinding
 import com.example.apka.databinding.ActivityUtworzElementBinding
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
 class UtworzElement : AppCompatActivity() {
     lateinit var binding: ActivityUtworzElementBinding
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Inicjalizacja binding
@@ -20,49 +26,73 @@ class UtworzElement : AppCompatActivity() {
         val DaneAplikacjiZmienna = DaneAplikacji()
         var aktywnaPodroz: Podroz = DaneAplikacjiZmienna.Podroze[IDPodrozy]
 
-        for (item in DaneAplikacjiZmienna.Podroze)
-        {
-            if(item.ID==IDPodrozy) {
+        var NazwaPrzedmiotuDoDodania = binding.NazwaElementu.text.toString()
+        var LiczbaPrzedmiotuDoDodania = ""
+
+        for (item in DaneAplikacjiZmienna.Podroze) {
+            if (item.ID == IDPodrozy) {
                 aktywnaPodroz = item
             }
         }
 
-        binding.DodajPrzedmiot.setOnClickListener {
-
-            var NazwaPrzedmiotuDoDodania = binding.NazwaElementu.text.toString()
-            //TODO zrobić to na boola
-            //var CzyLiczbaZaleznaOdDlugosciPodrozyDoDodania = binding.CzyLiczbaZaleznaOdDlugosciPodrozy.text.toString()
-            var LiczbaPrzedmiotuDoDodania = ""
-            if(binding.CzyLiczbaZaleznaOdDlugosciPodrozy.isChecked()){
+        binding.CzyLiczbaZaleznaOdDlugosciPodrozy.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                var CzyLiczbaZaleznaOdDlugosciPodrozyDoDodania = true
+                val dataRozpoczeciaFormat = LocalDate.parse(
+                    intent.getStringExtra("DATA_ROZPOCZECIA"),
+                    DateTimeFormatter.ofPattern("dd-MM-yyyy")
+                )
+                val dataZakonczeniaFormat = LocalDate.parse(
+                    intent.getStringExtra("DATA_ZAKONCZENIA"),
+                    DateTimeFormatter.ofPattern("dd-MM-yyyy")
+                )
+                var dni = ChronoUnit.DAYS.between(dataRozpoczeciaFormat, dataZakonczeniaFormat)
+                LiczbaPrzedmiotuDoDodania = dni.toString()
+                binding.LiczbaPrzedmiotow.setEnabled(false);
+                binding.LiczbaPrzedmiotow.setCursorVisible(false);
+                binding.LiczbaPrzedmiotow.setText(dni.toString())
+            }
+            else {
+                binding.LiczbaPrzedmiotow.setEnabled(true);
+                binding.LiczbaPrzedmiotow.setCursorVisible(true);
                 var CzyLiczbaZaleznaOdDlugosciPodrozyDoDodania = false
                 LiczbaPrzedmiotuDoDodania = binding.LiczbaPrzedmiotow.text.toString()
             }
-            else{
-                var CzyLiczbaZaleznaOdDlugosciPodrozyDoDodania = true
-                LiczbaPrzedmiotuDoDodania = binding.LiczbaPrzedmiotow.text.toString()
-            }
 
-            if(NazwaPrzedmiotuDoDodania == ""
-                || LiczbaPrzedmiotuDoDodania == "") {
-                Toast.makeText(this, "Wszystkie pola muszą zostać uzupełnione", Toast.LENGTH_LONG).show();
-            }
-            else {
-                Toast.makeText(this, "Dodano przedmiot do listy", Toast.LENGTH_LONG).show();
+            binding.DodajPrzedmiot.setOnClickListener {
 
-                var nowyPrzedmiot =
-                    Przedmiot(NazwaPrzedmiotuDoDodania, LiczbaPrzedmiotuDoDodania.toInt(), false, false)
-                //TODO OGARNAĆ CZEMU SIĘ NIE DODAJE
-                aktywnaPodroz.ListaPrzedmiotow.add(nowyPrzedmiot)
+                if (NazwaPrzedmiotuDoDodania == ""
+                    || LiczbaPrzedmiotuDoDodania == ""
+                ) {
+                    Toast.makeText(
+                        this,
+                        "Wszystkie pola muszą zostać uzupełnione",
+                        Toast.LENGTH_LONG
+                    ).show();
+                } else {
+                    //Toast.makeText(this, "Dodano przedmiot do listy", Toast.LENGTH_LONG).show();
+
+                    var nowyPrzedmiot =
+                        Przedmiot(
+                            NazwaPrzedmiotuDoDodania,
+                            LiczbaPrzedmiotuDoDodania.toInt(),
+                            false,
+                            false
+                        )
+
+                    //TODO OGARNAĆ CZEMU SIĘ NIE DODAJE aaaaaaaaaaaaaaaa
+                    aktywnaPodroz.ListaPrzedmiotow.add(nowyPrzedmiot)
 
 
-                val intencja = Intent(applicationContext, AktywnaPodroz::class.java)
-                intencja.putExtra("ID", intent.getIntExtra("ID", 1))
-                intencja.putExtra("NAZWA", intent.getStringExtra("NAZWA"))
-                intencja.putExtra("DATA_ROZPOCZECIA", intent.getStringExtra("DATA_ROZPOCZECIA"))
-                intencja.putExtra("DATA_ZAKONCZENIA", intent.getStringExtra("DATA_ZAKONCZENIA"))
-                intencja.putExtra("MIEJSCOWOSC", intent.getStringExtra("MIEJSCOWOSC"))
-                intencja.putExtra("TYP_PODROZY", intent.getStringExtra("TYP_PODROZY"))
-                startActivity(intencja)
+                    val intencja = Intent(applicationContext, AktywnaPodroz::class.java)
+                    intencja.putExtra("ID", intent.getIntExtra("ID", 1))
+                    intencja.putExtra("NAZWA", intent.getStringExtra("NAZWA"))
+                    intencja.putExtra("DATA_ROZPOCZECIA", intent.getStringExtra("DATA_ROZPOCZECIA"))
+                    intencja.putExtra("DATA_ZAKONCZENIA", intent.getStringExtra("DATA_ZAKONCZENIA"))
+                    intencja.putExtra("MIEJSCOWOSC", intent.getStringExtra("MIEJSCOWOSC"))
+                    intencja.putExtra("TYP_PODROZY", intent.getStringExtra("TYP_PODROZY"))
+                    startActivity(intencja)
+                }
             }
         }
     }
